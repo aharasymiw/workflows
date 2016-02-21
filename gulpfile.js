200856ae2d61c453e52abda19a1ee86b8293231c
Andrew Harasymiw <aharasymiw@gmail.com>
2016-02-21 13:06:41 -0600

    Added CoffeeScript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,21 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var coffee = require('gulp-coffee');
 
-gulp.task('log', function() {
-  gutil.log('Workflows are awesome');
+// Instantiate an array variable with source paths, best practice
+// Alternatively 'components/coffee/*.coffee'
+var coffeeSources = ['components/coffee/tagline.coffee']
+
+// A gulp task ('name', annonFunc(){})
+// 'gulp coffee' runs it in term, ie 'gulp "taskName"'
+gulp.task('coffee', function() {
+  //specify where file(s) to be processed are
+  //Can use a string literal, array of paths, or wildcard srouces
+  gulp.src(coffeeSources)
+    //.pipe it to another plugin, set options plugin({config})
+    .pipe(coffee({bare: true})
+      //coffee throws an error if problems, so catch for errors
+      .on('error', gutil.log)) // this is all in pipe
+    //.pipe results of coffee output to destination
+    .pipe(gulp.dest('components/scripts'))
 });
